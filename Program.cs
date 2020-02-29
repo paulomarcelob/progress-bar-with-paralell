b7909bb7084e809d00cd75ed874ce86974674ff6
Paulo <paulomarcelo@live.com>
2020-02-29 20:23:13 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgressBar_With_Paralell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var progress = new ProgressBar())
+            {
+                int counter = 0;
+                int total = 1000;
+                Parallel.For(0, total, new ParallelOptions { MaxDegreeOfParallelism = 4 }, i =>
+                {
+                    Interlocked.Increment(ref counter);
+                    progress.Report(counter, total);
+                    Thread.Sleep(100);
+                });
+            }
+        }
+    }
+}
